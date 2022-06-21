9d714c254fe3649be25d4c8b9326ab1524d543fb
IsaiasQB <isaias.qb@hotmail.com>
2022-06-21 14:20:27 -0400

    added webpack functionality to the code

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require("path");
+//bringing webpack's methods and properties into the config file. 
+const webpack  = require("webpack");
+
+
+//create the main configuration object within our file. Options within this object tell webpack what to do. 
+module.exports = {
+//For a basic configuration, we need to provide webpack with three properties: entry, output, and mode.
+
+  //The entry point is the root of the bundle and the beginning of the dependency graph
+  entry: './assets/js/script.js',
+
+  // entry code will be bundled and output to a folder that we specify.
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.bundle.js'
+  },
+
+  //if we want webpack to now use the jQuery package, we need to use a plugin to let webpack know
+  plugins: [
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jquery: 'jquery'
+    }),
+  ],
+  mode: 'development'
+}
